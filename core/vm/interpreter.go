72c67813ea7775cb940ba47331b331f9ebd9a322
skosito <skostic9242@gmail.com>
2024-09-03 11:18:49 +0200

    test

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -17,6 +17,7 @@
 package vm
 
 import (
+	"fmt"
 	"hash"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -187,7 +188,9 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		// Get the operation from the jump table and validate the stack to ensure there are
 		// enough stack items available to perform the operation.
 		op = contract.GetOp(pc)
+		fmt.Println("OP CODE ", op.String())
 		operation := in.cfg.JumpTable[op]
+		fmt.Println("OPERATION", operation)
 		cost = operation.constantGas // For tracing
 		// Validate stack
 		if sLen := stack.len(); sLen < operation.minStack {
@@ -196,9 +199,11 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 			return nil, &ErrStackOverflow{stackLen: sLen, limit: operation.maxStack}
 		}
 		if !contract.UseGas(cost) {
+			fmt.Println("ERROUTOFGAS")
 			return nil, ErrOutOfGas
 		}
 		if operation.dynamicGas != nil {
+			fmt.Println("DYNAMICGAS")
 			// All ops with a dynamic memory usage also has a dynamic gas cost.
 			var memorySize uint64
 			// calculate the new memory size and expand the memory to fit
@@ -239,6 +244,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		// execute the operation
 		res, err = operation.execute(&pc, in, callContext)
 		if err != nil {
+			fmt.Println("OPERATION EXECUTE ERROR ", err.Error())
 			break
 		}
 		pc++
